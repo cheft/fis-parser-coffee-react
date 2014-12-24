b927530864d5731420f1ade05ce9c11604a63317
陈海峰 <cn.cheft@gamil.com>
2014-12-24 11:58:30 +0800

    v0.0.8

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 'use strict';
 
-var fs = require('fs');
-
 var transform = require('coffee-react-transform');
 var coffee = require('coffee-script');
 
 module.exports = function(content, file, conf){
-	var ccode = transform(fs.readFileSync(conf.filename, 'utf-8'));
+	var ccode = transform(content);
     return coffee.compile(ccode);
 };
