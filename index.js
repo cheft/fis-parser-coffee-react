50fec08c6d3e99121c8daee7b2537749e00390b3
陈海峰 <cn.cheft@gamil.com>
2014-12-26 17:00:32 +0800

    add coffee compile config

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var coffee = require('coffee-script');
 
 module.exports = function(content, file, conf){
 	var ccode = transform(content);
-    return coffee.compile(ccode);
+    return coffee.compile(ccode, conf);
 };
